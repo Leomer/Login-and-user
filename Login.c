afbc2c01511c75bbe8e3e8d2ed06738ca2340923
Leomer <leomer331ff@hotmail.com>
2021-09-01 12:04:38 -0500

    Login and User

--- Login.c
+++ Login.c
@@ -3,21 +3,22 @@
 #include<stdlib.h>
 #include<conio.h>
 #include<ctype.h>
+#include<time.h>
 
 #define USER "leomer"
 #define PASS "Leo123"
 
 int main(void) {
-    char user[20], pass[20], letra;
-    int status = 0, intento = 0;
+    char user[20], pass[20], letter;
+    int status = 0, chance = 0;
     do {
         int i = 0, j=0;
         system("cls");
-        printf("\t\t================\n");
-        printf("\t\t=  Bienvenido  =\n");
-        printf("\t\t================\n\n");
+        printf("\t\t=============\n");
+        printf("\t\t=  Welcome  =\n");
+        printf("\t\t=============\n\n");
 
-        printf("\tUsuario: ");
+        printf("\tUser: ");
         gets(user);
         while(user[j] != '\0'){
             user[j] = tolower(user[j]);
@@ -25,34 +26,38 @@ int main(void) {
         }
 
         printf("\tPassword: ");
-        while(letra = getch()) {
-            if(letra == 13) {
+        while(letter = getch()) {
+            if(letter == 13) {
                 pass[i] = '\0';
                 break;
-            } else if(letra == 8) {
+            } else if(letter == 8) {
                 if(i > 0) {
                     i--;
                     printf("\b \b");
                 }
             } else {
                 printf("*");
-                pass[i] = letra;
+                pass[i] = letter;
                 i++;
             }
         }
 
-
         if(strcmp(user, USER) == 0 && strcmp(pass, PASS) == 0) {
             status = 1;
         } else {
-            printf("Error en el acceso");
-            intento++;
+            printf("\n\n\tError");
+            chance++;
+            clock_t delay = clock();
+            while(clock() < delay + 1500);
         }
 
-    } while(intento < 3 && status == 0);
+    } while(chance < 3 && status == 0);
     system("cls");
     if(status == 1) {
-        printf("Acceso concedido");
+        printf("Access Granted");
+    }else{
+        printf("Access Denied");
     }
+
     return 0;
 }
